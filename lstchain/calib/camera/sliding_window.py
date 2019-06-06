7cd13f31b24184685d3a21036afb4bb829ae5cbc
Yukiho Kobayashi <yukihok@icrhome02.icrr.u-tokyo.ac.jp>
2019-06-06 09:02:20 +0900

    modify sliding window for the latest ctapipe

--- lstchain/calib/camera/sliding_window.py
+++ lstchain/calib/camera/sliding_window.py
@@ -10,16 +10,11 @@ class PulseIntegrator():
         self.num_gains = 2
         self.num_pixels = 1855
 
-    def sliding_window(self, event, slide_width):
+    def sliding_window(self, event, slide_width, window_width):
         
-        window_width = 3
-        if slide_width % 2 == 0:
-            print('Error: Please set odd number for window_width.')
-            return
-        width = (slide_width - 1) / 2
+        full_width = slide_width * 2 + 1
         waveforms = event.r1.tel[self.tel_id].waveform[:, :, 2:38]
         peakpos = np.argmax(waveforms, axis = 2)
-        event.dl1.tel[self.tel_id].peakpos = peakpos
         slide_start = peakpos - self.start_offset
         slide_time = 10
         ind = np.indices(waveforms.shape)[2]
@@ -27,14 +22,13 @@ class PulseIntegrator():
         window_center = np.zeros((self.num_gains, self.num_pixels))
         for num_slide in range(0, slide_time):
             front = slide_start + num_slide
-            end = front + slide_width
+            end = front + full_width
             slide_window = (ind >= front[..., None]) & (ind < end[..., None])
             cur_slide_sum = np.sum(waveforms * slide_window, axis = 2)
             replaced = cur_slide_sum > slide_max
-            window_center = window_center * ~replaced + (front + width) * replaced
+            window_center = window_center * ~replaced + (front + slide_width) * replaced
             slide_max = slide_max * ~replaced + cur_slide_sum * replaced
 
         window = (ind >= window_center[..., None] - window_width) & (ind <= window_center[..., None] + window_width)
-        event.dl1.tel[self.tel_id].peakpos = window_center
-        event.dl1.tel[self.tel_id].extracted_samples = window
+        return window
         
